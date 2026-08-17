34844e5efb64cba800a6942c79ef699738a25987
apiolli <cesardiaz.miduho@gmail.com>
2026-08-17 14:20:45 -0400

    feat: add CustomNavigation component for the app navigation

---
+++ heroes-app/src/components/custom/CustomMenu.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@/lib/utils";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "../ui/navigation-menu";
+import { Link, useLocation } from "react-router";
+
+export const CustomMenu = () => {
+  const { pathname } = useLocation();
+
+  const isActive = (path: string) => pathname === path;
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {/* Home */}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            render={<Link to="/" />}
+            className={cn(isActive("/") && "bg-slate-200", "rounded-md p-2")}
+          >
+            Inicio
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        {/* Search */}
+
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            render={<Link to="/search" />}
+            className={cn(
+              isActive("/search") && "bg-slate-200",
+              "rounded-md p-2",
+            )}
+          >
+            Buscar superheroes
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
