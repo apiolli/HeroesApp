c793b574b6c20678bcc98ca80a437990caec3170
apiolli <cesardiaz.miduho@gmail.com>
2026-09-08 22:31:02 -0400

    feat: implement search functionality with advanced filters and accordion UI

--- heroes-app/src/herores/pages/search/SearchPage.tsx
+++ heroes-app/src/herores/pages/search/SearchPage.tsx
@@ -2,8 +2,22 @@ import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "@/herores/components/HeroStats";
 import { SearchControls } from "./ui/SearchControls";
 import { CustomBreadCrumbs } from "@/components/custom/CustomBreadCrumbs";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router";
+import { searchHeroesAction } from "@/herores/actions/search-heros.action";
+import { HeroGrid } from "@/herores/components/HeroGrid";
 
 export const SearchPage = () => {
+  const [searchParams] = useSearchParams();
+
+  const name = searchParams.get("name") ?? undefined;
+  const strength = searchParams.get("strength") ?? undefined;
+
+  const { data: heroes = [] } = useQuery({
+    queryKey: ["search", { name, strength }],
+    queryFn: () => searchHeroesAction({ name, strength }),
+    staleTime: 1000 * 60 * 5,
+  });
   return (
     <>
       <CustomJumbotron
@@ -13,6 +27,7 @@ export const SearchPage = () => {
       <CustomBreadCrumbs currentPage="Buscador de heroes" breadCrumbs={[]} />
       <HeroStats />
       <SearchControls />
+      <HeroGrid heroes={heroes} />
     </>
   );
 };
